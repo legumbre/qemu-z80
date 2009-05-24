5cd6e74a378995de4043dbc85bc69312ee30b01b
Stuart Brady <stuart.brady@gmail.com>
2009-05-24 09:58:35 +0100

    Add missing file

---
+++ hw/zx_key_template.h
@@ -0,0 +1,50 @@
+/*
+ * ZX Spectrum Keyboard Layout
+ */
+
+        /* Name,    Row, Column */
+DEF_ZX_KEY(1,         3, 0)
+DEF_ZX_KEY(2,         3, 1)
+DEF_ZX_KEY(3,         3, 2)
+DEF_ZX_KEY(4,         3, 3)
+DEF_ZX_KEY(5,         3, 4)
+DEF_ZX_KEY(6,         4, 4)
+DEF_ZX_KEY(7,         4, 3)
+DEF_ZX_KEY(8,         4, 2)
+DEF_ZX_KEY(9,         4, 1)
+DEF_ZX_KEY(0,         4, 0)
+
+DEF_ZX_KEY(Q,         2, 0)
+DEF_ZX_KEY(W,         2, 1)
+DEF_ZX_KEY(E,         2, 2)
+DEF_ZX_KEY(R,         2, 3)
+DEF_ZX_KEY(T,         2, 4)
+DEF_ZX_KEY(Y,         5, 4)
+DEF_ZX_KEY(U,         5, 3)
+DEF_ZX_KEY(I,         5, 2)
+DEF_ZX_KEY(O,         5, 1)
+DEF_ZX_KEY(P,         5, 0)
+
+DEF_ZX_KEY(A,         1, 0)
+DEF_ZX_KEY(S,         1, 1)
+DEF_ZX_KEY(D,         1, 2)
+DEF_ZX_KEY(F,         1, 3)
+DEF_ZX_KEY(G,         1, 4)
+DEF_ZX_KEY(H,         6, 4)
+DEF_ZX_KEY(J,         6, 3)
+DEF_ZX_KEY(K,         6, 2)
+DEF_ZX_KEY(L,         6, 1)
+DEF_ZX_KEY(ENTER,     6, 0)
+
+DEF_ZX_KEY(CAPSSHIFT, 0, 0)
+DEF_ZX_KEY(Z,         0, 1)
+DEF_ZX_KEY(X,         0, 2)
+DEF_ZX_KEY(C,         0, 3)
+DEF_ZX_KEY(V,         0, 4)
+DEF_ZX_KEY(B,         7, 4)
+DEF_ZX_KEY(N,         7, 3)
+DEF_ZX_KEY(M,         7, 2)
+DEF_ZX_KEY(SYMBSHIFT, 7, 1)
+DEF_ZX_KEY(SPACE,     7, 0)
+
+#undef DEF_ZX_KEY
