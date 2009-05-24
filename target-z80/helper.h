5734426f2941d09462a3d282c77c577775934f8c
Stuart Brady <stuart.brady@gmail.com>
2009-05-24 20:11:04 +0100

    Add comments to helper declarations

--- target-z80/helper.h
+++ target-z80/helper.h
@@ -32,15 +32,21 @@ DEF_HELPER_0_0(set_inhibit_irq, void, (void))
 DEF_HELPER_0_0(reset_inhibit_irq, void, (void))
 
 DEF_HELPER_0_1(movl_pc_im, void, (uint32_t))
+
 DEF_HELPER_0_0(halt, void, (void))
+
+/* In / Out */
 DEF_HELPER_0_1(in_T0_im, void, (uint32_t))
 DEF_HELPER_0_0(in_T0_bc_cc, void, (void))
 DEF_HELPER_0_1(out_T0_im, void, (uint32_t))
 DEF_HELPER_0_0(out_T0_bc, void, (void))
 
+/* Misc */
 DEF_HELPER_0_1(bit_T0, void, (uint32_t))
 DEF_HELPER_0_0(jmp_T0, void, (void))
 DEF_HELPER_0_2(djnz, void, (uint32_t, uint32_t))
+
+/* 8-bit arithmetic */
 DEF_HELPER_0_0(add_cc, void, (void))
 DEF_HELPER_0_0(adc_cc, void, (void))
 DEF_HELPER_0_0(sub_cc, void, (void))
@@ -49,6 +55,8 @@ DEF_HELPER_0_0(and_cc, void, (void))
 DEF_HELPER_0_0(xor_cc, void, (void))
 DEF_HELPER_0_0(or_cc, void, (void))
 DEF_HELPER_0_0(cp_cc, void, (void))
+
+/* Rotation/shifts */
 DEF_HELPER_0_0(rlc_T0_cc, void, (void))
 DEF_HELPER_0_0(rrc_T0_cc, void, (void))
 DEF_HELPER_0_0(rl_T0_cc, void, (void))
@@ -59,6 +67,8 @@ DEF_HELPER_0_0(sll_T0_cc, void, (void))
 DEF_HELPER_0_0(srl_T0_cc, void, (void))
 DEF_HELPER_0_0(rld_cc, void, (void))
 DEF_HELPER_0_0(rrd_cc, void, (void))
+
+/* Block instructions */
 DEF_HELPER_0_0(bli_ld_inc_cc, void, (void))
 DEF_HELPER_0_0(bli_ld_dec_cc, void, (void))
 DEF_HELPER_0_1(bli_ld_rep, void, (uint32_t))
@@ -69,6 +79,8 @@ DEF_HELPER_0_1(bli_cp_rep, void, (uint32_t))
 DEF_HELPER_0_0(bli_io_inc, void, (void))
 DEF_HELPER_0_0(bli_io_dec, void, (void))
 DEF_HELPER_0_1(bli_io_rep, void, (uint32_t))
+
+/* Misc */
 DEF_HELPER_0_0(rlca_cc, void, (void))
 DEF_HELPER_0_0(rrca_cc, void, (void))
 DEF_HELPER_0_0(rla_cc, void, (void))
@@ -78,11 +90,15 @@ DEF_HELPER_0_0(cpl_cc, void, (void))
 DEF_HELPER_0_0(scf_cc, void, (void))
 DEF_HELPER_0_0(ccf_cc, void, (void))
 DEF_HELPER_0_0(neg_cc, void, (void))
+
+/* 16-bit arithmetic */
 DEF_HELPER_0_0(sbcw_T0_T1_cc, void, (void))
 DEF_HELPER_0_0(addw_T0_T1_cc, void, (void))
 DEF_HELPER_0_0(adcw_T0_T1_cc, void, (void))
 DEF_HELPER_0_0(incb_T0_cc, void, (void))
 DEF_HELPER_0_0(decb_T0_cc, void, (void))
+
+/* Interrupt handling / IR registers */
 DEF_HELPER_0_1(imode, void, (int))
 DEF_HELPER_0_0(ei, void, (void))
 DEF_HELPER_0_0(di, void, (void))
@@ -91,6 +107,8 @@ DEF_HELPER_0_0(ld_R_A, void, (void))
 DEF_HELPER_0_0(ld_I_A, void, (void))
 DEF_HELPER_0_0(ld_A_R, void, (void))
 DEF_HELPER_0_0(ld_A_I, void, (void))
+
+/* R800 */
 DEF_HELPER_0_0(mulub_cc, void, (void))
 DEF_HELPER_0_0(muluw_cc, void, (void))
 
